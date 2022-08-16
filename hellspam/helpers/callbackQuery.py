ba31b5b4f9478241bb0d76a44576e2c07e743e03
Akhil <98938102+akhilprs@users.noreply.github.com>
2022-08-16 17:01:54 +0530

    Update callbackQuery.py

--- hellspam/helpers/callbackQuery.py
+++ hellspam/helpers/callbackQuery.py
@@ -23,3 +23,25 @@ async def no(e):
         await e.answer("Only Owner, Co-Owner And Sudo Users Can Access This Buttons!", cache_time=0, alert=True)
     else:
         await e.edit(f"{ALIVE_CMD}", buttons=BACK)
+
+
+
+@SpamBot1.on(events.CallbackQuery(data=b'ping'))
+@SpamBot2.on(events.CallbackQuery(data=b'ping'))
+@SpamBot3.on(events.CallbackQuery(data=b'ping'))
+@SpamBot4.on(events.CallbackQuery(data=b'ping'))
+@SpamBot5.on(events.CallbackQuery(data=b'ping'))
+@SpamBot6.on(events.CallbackQuery(data=b'ping'))
+@SpamBot7.on(events.CallbackQuery(data=b'ping'))
+@SpamBot8.on(events.CallbackQuery(data=b'ping'))
+@SpamBot9.on(events.CallbackQuery(data=b'ping'))
+@SpamBot10.on(events.CallbackQuery(data=b'ping'))
+
+
+
+
+async def no(e):
+    if e.query.user_id not in MY_USERS:
+        await e.answer("Only Owner, Co-Owner And Sudo Users Can Access This Buttons!", cache_time=0, alert=True)
+    else:
+        await e.edit(f"{PING_CMD}", buttons=BACK)
