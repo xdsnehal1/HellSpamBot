3035507a878cf1d1eb2f9c257d45dca3e0e9906a
Akhil <98938102+akhilprs@users.noreply.github.com>
2022-08-16 16:51:11 +0530

    Create help.py

---
+++ hellspam/help.py
@@ -0,0 +1,40 @@
+from hellspam import *
+from hellspam import SpamBot1, SpamBot2, SpamBot3, SpamBot4, SoamBot5, SpamBot6, SpamBot7, SpamBot8, SpamBot9, SpamBot10
+from hellspam.helpers.commands import *
+from telethon import events, Button
+
+
+Buttons = [
+    Button.inline("Alive", b'alive'),
+    Button.inline("Ping", b'ping')
+], [
+    Button.inline("Raid", b'raid'),
+    Button.inline("Reply Raid", b'replyraid')
+], [
+    Button.inline("Spam", b'spam'),
+    Button.inline("Pspam", b'pspam')
+], [
+    Button.inline("Extras", b'extras')
+], [
+    Button.url("Channel", "t.me/HellSpamBot"),
+    Button.url("Group", "t.me/HellSpam_SupportChat")
+]
+
+BACK = [
+    Button.inline("Back", b'back')
+]
+
+@SpamBot1.on(events.NewMessage(incoming=True, pattern='/help'))
+@SpamBot2.on(events.NewMessage(incoming=True, pattern='/help'))
+@SpamBot3.on(events.NewMessage(incoming=True, pattern='/help'))
+@SpamBot4.on(events.NewMessage(incoming=True, pattern='/help'))
+@SpamBot5.on(events.NewMessage(incoming=True, pattern='/help'))
+@SpamBot6.on(events.NewMessage(incoming=True, pattern='/help'))
+@SpamBot7.on(events.NewMessage(incoming=True, pattern='/help'))
+@SpamBot8.on(events.NewMessage(incoming=True, pattern='/help'))
+@SpamBot9.on(events.NewMessage(incoming=True, pattern='/help'))
+@SpamBot10.on(events.NewMessage(incoming=True, pattern='/help'))
+
+async def help(e):
+    if e.sender_id in MY_USERS:
+        message = await e.client.send_file(e.chat_id, DISPLAY_PIC, caption="This Is Help Command!!!", buttons=Buttons)
