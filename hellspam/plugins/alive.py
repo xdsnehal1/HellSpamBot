a82a456be235012ed91bd6e8dbfbf86f80aedd0e
Akhil <98938102+akhilprs@users.noreply.github.com>
2022-08-16 17:23:47 +0530

    Create alive.py

---
+++ hellspam/plugins/alive.py
@@ -0,0 +1,35 @@
+from hellspam import *
+from hellspam import SpamBot1, SpamBot2, SpamBot3, SpamBot4, SpamBot5, SpamBot6, SpamBot7, SpamBot8, SpamBot9, SpamBot10 
+from telethon import events
+from telethon import version
+
+
+master = f"[{OWNER_NAME}](tg://user?id={OWNER_ID})"
+
+
+alive_msg = f"""
+Hell Spam Bot Is Alive!
+My Master:- {master}
+Bot Version:- `{BOT_VERSION}`
+Telethon Version:- `{version.__version__}`
+{BIO_MSG}
+
+© @HellSpamBot
+"""
+
+@SpamBot1.on(events.NewMessage(incoming=True, pattern='/alive'))
+@SpamBot2.on(events.NewMessage(incoming=True, pattern='/alive'))
+@SpamBot3.on(events.NewMessage(incoming=True, pattern='/alive'))
+@SpamBot4.on(events.NewMessage(incoming=True, pattern='/alive'))
+@SpamBot5.on(events.NewMessage(incoming=True, pattern='/alive'))
+@SpamBot6.on(events.NewMessage(incoming=True, pattern='/alive'))
+@SpamBot7.on(events.NewMessage(incoming=True, pattern='/alive'))
+@SpamBot8.on(events.NewMessage(incoming=True, pattern='/alive'))
+@SpamBot9.on(events.NewMessage(incoming=True, pattern='/alive'))
+@SpamBot10.on(events.NewMessage(incoming=True, pattern='/alive'))
+async def alive(e):
+    if e.sender_id in MY_USERS:
+        try:
+            await e.client.send_file(e.chat_id, DISPLAY_PIC, caption=alive_msg)
+        except Exception as e:
+            print(e)
