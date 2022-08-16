abb69abf889ea6799fff4f5b1a2f61da57055b8f
Akhil <98938102+akhilprs@users.noreply.github.com>
2022-08-16 16:37:09 +0530

    Update __main__.py

--- hellspam/__main__.py
+++ hellspam/__main__.py
@@ -4,21 +4,21 @@ import importlib
 import logging
 from pathlib import Path
 from sys import argv
-from spambot import *
-from spambot import MafiaBot1, MafiaBot2, MafiaBot3, MafiaBot4, MafiaBot5
+from hellspam import *
+from hellspam import SpamBot1, SpamBot2, SpamBot3, SpamBot4, SpamBot5, SpamBot6, SpamBot7, SpamBot8, SpamBot9, SpamBot10
 
 def load_plugs(plugname):
-    modules = Path(f"spambot/plugins/{plugname}.py")
-    myfiles = f"spambot.plugins.{plugname}"
+    modules = Path(f"hellspam.plugins/{plugname}.py")
+    myfiles = f"hellspam.plugins.{plugname}"
     spec = importlib.util.spec_from_file_location(myfiles, modules)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugname)
     spec.loader.exec_module(load)
     sys.modules["spambot.plugins." + plugname] = load
-    print("MafiaSpamBot - Successfully Imported " + plugname)
+    print("Hell SpamBot - Successfully Imported " + plugname)
 
 if __name__ == "__main__":
-    modules = "spambot/plugins/*.py"
+    modules = "hellspam/plugins/*.py"
     plugins = glob.glob(modules)
     for myfiles in plugins:
         with open(myfiles) as f:
