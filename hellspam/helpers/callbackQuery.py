fb45f18283fd854de9d55a18780d3af23e22e5b6
Akhil <98938102+akhilprs@users.noreply.github.com>
2022-08-16 16:58:35 +0530

    Create callbackQuery.py

---
+++ hellspam/helpers/callbackQuery.py
@@ -0,0 +1,25 @@
+from hellspam import *
+from hellspam import SpamBot1, SpamBot2, SpamBot3, SpamBot4, SpamBot5, SpamBot6, SpamBot7, SpamBot8, SpamBot9, SpamBot10 
+from hellspam.help import *
+from hellspam.helpers.commands import *
+from telethon import events
+
+
+@SpamBot1.on(events.CallbackQuery(data=b'alive'))
+@SpamBot2.on(events.CallbackQuery(data=b'alive'))
+@SpamBot3.on(events.CallbackQuery(data=b'alive'))
+@SpamBot4.on(events.CallbackQuery(data=b'alive'))
+@SpamBot5.on(events.CallbackQuery(data=b'alive'))
+@SpamBot6.on(events.CallbackQuery(data=b'alive'))
+@SpamBot7.on(events.CallbackQuery(data=b'alive'))
+@SpamBot8.on(events.CallbackQuery(data=b'alive'))
+@SpamBot9.on(events.CallbackQuery(data=b'alive'))
+@SpamBot10.on(events.CallbackQuery(data=b'alive'))
+
+
+
+async def no(e):
+    if e.query.user_id not in MY_USERS:
+        await e.answer("Only Owner, Co-Owner And Sudo Users Can Access This Buttons!", cache_time=0, alert=True)
+    else:
+        await e.edit(f"{ALIVE_CMD}", buttons=BACK)
